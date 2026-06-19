3bc915a67eb70e1f5296fa9979c5f5af77601719
Sanjeev Midda <s.midda1@gmail.com>
2026-06-19 14:53:41 +0100

    feat(app): render todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import "./index.css";
+import Todo from "./components/todo";
+import todoData from "./data/todoData";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {todoData.map((i) => {
+        return (
+          <Todo
+            key={i.id}
+            todoName={i.taskName}
+            completionStatus={i.completed}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
