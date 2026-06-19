fe85da77be5d4e5eb948066d58b131898a9d36c2
Sanjeev Midda <s.midda1@gmail.com>
2026-06-19 14:00:12 +0100

    feat: create todos intial data

---
+++ src/data/todoData.tsx
@@ -0,0 +1,29 @@
+const todoTasks = [
+  {
+    id: 0,
+    taskName: "take umbrella",
+    completed: false,
+  },
+  {
+    id: 1,
+    taskName: "go running",
+    completed: false,
+  },
+  {
+    id: 2,
+    taskName: "begin reading chapter one from Focus",
+    completed: false,
+  },
+  {
+    id: 3,
+    taskName: "Go to New York",
+    completed: false,
+  },
+  {
+    id: 4,
+    taskName: "Build table",
+    completed: false,
+  },
+];
+
+export default todoTasks;
